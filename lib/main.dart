9fa68a8e56c6c3849c3e71790cb2d9b6f1997dec
sky4564 <qw486512@gmail.com>
2024-09-07 22:31:04 +0900

    api 비동기 통신에 따른 로딩표시 with future builder widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/services/api_service.dart';
 // import 'package:untitled/widgets/button.dart';/
-import 'package:untitled/widgets/welcometext.dart';
+import 'package:untitled/widgets/welcome_text.dart';
+import 'package:untitled/services/api_service.dart';
+import 'package:untitled/models/webtoon_model.dart';
 
 void main() {
-  ApiService().getTodaysToons();
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
+  App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 80,
+    return FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const MaterialApp(
+              home: Scaffold(
+                backgroundColor: Color(0xFF181818),
+                body: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 80,
+                      ),
+
+                      WelcomeT(user: 'kaki'),
+
+                      SizedBox(
+                        height: 120,
+                        width: 30,
+                      ),
+
+                      Text(
+                        'Total Balance',
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Color.fromARGB(255, 100, 58, 58),
+                        ),
+                      ),
+
+                      // SizedBox(
+                      //   height: 5,
+                      // ),
+
+                      // Text(
+                      //   '\$5 194 482',
+                      //   style: TextStyle(
+                      //     fontSize: 48,
+                      //     fontWeight: FontWeight.w600,
+                      //     color: Colors.white,
+                      //   ),
+                      // ),
+
+                      // SizedBox(
+                      //   height: 30,
+                      // ),
+
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //   children: [
+                      //     Button(
+                      //       text: 'Transfer',
+                      //       bgColor: Color(0xFFF1B33B),
+                      //       textColor: Colors.black,
+                      //     ),
+                      //     Button(
+                      //       text: 'Request',
+                      //       bgColor: Color(0xFF1F2123),
+                      //       textColor: Colors.white,
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
+                  ),
+                ),
               ),
-
-              WelcomeT(user: 'kaki'),
-
-              // const SizedBox(
-              //   height: 120,
-              //   width: 30,
-              // ),
-
-              // Text(
-              //   'Total Balance',
-              //   style: TextStyle(
-              //     fontSize: 22,
-              //     color: Colors.white.withOpacity(0.8),
-              //   ),
-              // ),
-
-              // SizedBox(
-              //   height: 5,
-              // ),
-
-              // Text(
-              //   '\$5 194 482',
-              //   style: TextStyle(
-              //     fontSize: 48,
-              //     fontWeight: FontWeight.w600,
-              //     color: Colors.white,
-              //   ),
-              // ),
-
-              // SizedBox(
-              //   height: 30,
-              // ),
-
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Button(
-              //       text: 'Transfer',
-              //       bgColor: Color(0xFFF1B33B),
-              //       textColor: Colors.black,
-              //     ),
-              //     Button(
-              //       text: 'Request',
-              //       bgColor: Color(0xFF1F2123),
-              //       textColor: Colors.white,
-              //     ),
-              //   ],
-              // ),
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as kaki_magic;
+import 'package:untitled/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     final url = Uri.parse('$baseUrl/$today');
     final response = await kaki_magic.get(url);
+
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
+      }
+
+      return webtoonInstances;
     }
+
     throw Error();
   }
 }
